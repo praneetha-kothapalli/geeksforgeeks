1fa9f815259c826a70d0a7192af73a8ed588b440
praneetha-kothapalli <kothapallipraneetha5@gmail.com>
2025-09-03 10:22:48 +0530

    Update normal.java

--- week1/prime-numbers/normal.java
+++ week1/prime-numbers/normal.java
@@ -1,3 +1,4 @@
+//program to certain number
 class PrimeCheck {
     static boolean isPrime(int n) {
         // 1 and below are not prime
@@ -23,3 +24,31 @@ class PrimeCheck {
             System.out.println(n + " is not a prime number.");
     }
 }
+
+//program for 1 to n
+class PrimeNumbers {
+    // Function to check prime
+    static boolean isPrime(int n) {
+        if (n <= 1) return false; // 0 and 1 are not prime
+        if (n == 2 || n == 3) return true;
+
+        // check divisibility from 2 to sqrt(n)
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0)
+                return false;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        int n = 50; // upper limit
+        System.out.println("Prime numbers from 1 to " + n + ":");
+
+        for (int i = 1; i <= n; i++) {
+            if (isPrime(i)) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+}
+
