79931d106f2dc9c5d07b1423ccf644e12a5d260e
praneetha-kothapalli <kothapallipraneetha5@gmail.com>
2025-09-04 10:26:00 +0530

    Update difference.java

--- week1/difference.java
+++ week1/difference.java
@@ -1,32 +1,18 @@
+class Hello{
 public double getAngle(String s) {
-
-
-// code here
-
-String[] time=s.split(":");
-
-int hour-Integer.parseInt(time[0]);
-
-int minute Integer.parseInt(time[1]);
-
-if (hour==0){
-
+  String[] time=s.split(":");
+  int hour-Integer.parseInt(time[0]);
+  int minute Integer.parseInt(time[1]);
+  if (hour==0){
+    hour=12;
+  }
+  else if (hour>12){
+    hour-=12;
+  }
+  double minuteAngle=minute*6;
+  double hourAngle=(hour*30)+(minute*0.5);
+  double angleDifference=Math.abs(hourAngle-minuteAngle);
+  return Math.min(angleDifference, 360-angleDifference);
 }
-
-hour=12;
-
-else if (hour>12){
-
 }
-
-hour-=12;
-
-double minuteAngle=minute*6;
-
-double hourAngle=(hour*30)+(minute*0.5);
-
-double angleDifference=Math.abs(hourAngle-minuteAngle);
-
-return Math.min(angleDifference, 360-angleDifference);
-
 //find the smallest difference between the hour and minute clock.
